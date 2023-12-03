7dfbc00a09024d0e2e1985356fd3887849c9ada7
jungwoo <kjw0323@gmail.com>
2023-12-03 20:22:54 +0900

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule AdventOfCode.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :advent_of_code,
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :inets]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:benchee, "~> 1.0"},
+      {:ssl_verify_fun, "~> 1.1.7", manager: :rebar3, override: true}
+    ]
+  end
+end
