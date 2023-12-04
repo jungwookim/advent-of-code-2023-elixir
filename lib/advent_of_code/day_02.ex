5e6cf440eb099c12b4ee6e54da6e2e1b276add85
jungwookim <kjw0323@gmail.com>
2023-12-04 22:06:26 +0900

    day02

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,75 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  defp get_game_id(bool, game_id) do
+    case bool do
+      true -> String.to_integer(game_id)
+      false -> 0
+    end
   end
 
-  def part2(_args) do
+  defp part1_logic(game) do
+    red = 12
+    green = 13
+    blue = 14
+    [game, info] = String.split(game, ": ", trim: true)
+    [_, game_id] = String.split(game, " ")
+    info
+    |> String.split("; ", trim: true)
+    |> Enum.map(fn set ->
+      String.split(set, ", ", trim: true)
+    end)
+    |> Enum.map(fn set ->
+      Enum.map(set, fn each ->
+        case String.split(each, " ", trim: true) do
+        [count, "red"] -> red >= String.to_integer(count)
+        [count, "green"] -> green >= String.to_integer(count)
+        [count, "blue"] -> blue >= String.to_integer(count)
+      end
+      end)
+    end)
+    |> List.flatten()
+    |> Enum.all?(fn x -> x == true end)
+    |> get_game_id(game_id)
+    |> dbg()
+  end
+
+  def part1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&part1_logic/1)
+    |> Enum.sum()
+    |> dbg()
+  end
+
+  defp part2_logic(game) do
+    [game, info] = String.split(game, ": ", trim: true)
+    [_, _game_id] = String.split(game, " ")
+    info
+    |> String.split("; ", trim: true)
+    |> Enum.map(fn set ->
+      String.split(set, ", ", trim: true)
+    end)
+    |> Enum.map(fn set ->
+      Enum.map(set, fn each ->
+        case String.split(each, " ", trim: true) do
+        [count, "red"] -> %{red: String.to_integer(count)}
+        [count, "green"] -> %{green: String.to_integer(count)}
+        [count, "blue"] -> %{blue: String.to_integer(count)}
+      end
+      end)
+    end)
+    |> List.flatten()
+    |> Enum.reduce(%{}, fn x, acc ->
+      Map.merge(acc, x, fn _, v1, v2 -> max(v1, v2) end)
+    end)
+    |> Map.values()
+    |> Enum.reduce(1, fn x, acc -> acc * x end)
+  end
+
+  def part2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&part2_logic/1)
+    |> Enum.sum()
+    |> dbg()
   end
 end
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -5,15 +5,14 @@ defmodule AdventOfCode.Day02Test do
 
   @tag :skip
   test "part1" do
-    input = nil
+    input = File.read!("priv/static/advent_of_code/input_02_part1.txt")
     result = part1(input)
 
     assert result
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = File.read!("priv/static/advent_of_code/input_02_part2.txt")
     result = part2(input)
 
     assert result
