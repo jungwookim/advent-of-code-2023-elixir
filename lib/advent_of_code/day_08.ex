17cc1e107042d6fcc8f7bd1ba4bb1196cf8c95b7
jungwookim <kjw0323@gmail.com>
2023-12-29 23:22:38 +0900

    day08

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,7 +1,68 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_args) do
+  defp dir_to_int(dir) do
+    case dir do
+      "R" -> 1
+      "L" -> 0
+    end
   end
 
-  def part2(_args) do
+  defp process(maps, infinite_sequence, position, num, terminate_fn) do
+    if terminate_fn.(position) do
+      dbg(num)
+    else
+      dir = Enum.at(infinite_sequence, num)
+      process(maps, infinite_sequence, elem(maps[position], dir_to_int(dir)), num + 1, terminate_fn)
+    end
+  end
+
+  defp parse_input(input) do
+    [instructions | for_maps] = String.split(input, "\n", trim: true)
+    {Stream.cycle(String.graphemes(instructions)), for_maps}
+  end
+
+  defp parse_maps(for_maps) do
+    Enum.reduce(for_maps, %{}, fn line, acc ->
+      [position, directions] =
+        String.split(
+          line
+          |> String.replace("(", "")
+          |> String.replace(")", ""),
+          " = ",
+          trim: true
+        )
+
+      [left, right] = String.split(directions, ", ", trim: true)
+
+      Map.put(acc, position, {left, right})
+    end)
+  end
+
+  def part1(input) do
+    {infinite_instructions, for_maps} = parse_input(input)
+    maps = parse_maps(for_maps)
+    start = "AAA"
+    process(maps, infinite_instructions, start, 0, &(&1 === "ZZZ"))
+  end
+
+  defp lcm(x, y) do
+    div(x * y, Integer.gcd(x, y))
+  end
+
+  def part2(input) do
+    {infinite_instructions, for_maps} = parse_input(input)
+    maps = parse_maps(for_maps)
+
+    starts =
+      maps
+      |> Map.keys()
+      |> Enum.map(fn key -> if String.ends_with?(key, "A"), do: key, else: nil end)
+      |> Enum.filter(&(&1 != nil))
+
+    starts
+    |> dbg()
+    |> Enum.map(fn start -> process(maps, infinite_instructions, start, 0, &(String.ends_with?(&1, "Z"))) end)
+    |> dbg()
+    |> Enum.reduce(1, &lcm/2)
+    |> dbg()
   end
 end
